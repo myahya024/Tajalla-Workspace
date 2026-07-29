ae4d09178ebc3c238ac84fbac73af18fd32e5813
myahya024 <myahya@tajalla.co>
2026-07-29 15:48:42 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = "https://fwqigsujabyoqmduoelx.supabase.co";
-const SUPABASE_KEY = "sb_publishable_G_rP3z1tvsndCP2GrF5AOA_V1UVAS3f"; 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-const SECRET_PIN = "$Ark0820";
-const SESSION_KEY = "app_login_session";
